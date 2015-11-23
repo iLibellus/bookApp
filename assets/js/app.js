55b75bca46861e1debe336664f0fc9ecd6f9b3c1
Elisabet Svärd <elisabet.svard@transmode.com>
2015-11-23 16:30:41 +0100

    Add route to bookInfo.

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,10 +6,16 @@ bookApp.config(['$routeProvider',
     $routeProvider.when('/', {
       templateUrl: '/templates/book.html',
       controller: 'BookCtrl'
-    }).otherwise({
-      redirectTo: '/',
-      caseInsensitiveMatch: true
-    })
+    }) //.when adds new route definition to $route service
+	   .when('/profile/:profileid',{
+	   			//Profile View Routing to profile.html and controller - ProfileController
+				templateUrl:'/templates/profile.html',
+				controller:'ProfileController'
+		})
+		.otherwise({
+		  redirectTo: '/',
+		  caseInsensitiveMatch: true
+		})
   }]);
   
   bookApp.controller('BookCtrl', ['$scope', '$rootScope', 'BookService', function($scope, $rootScope, BookService) {
@@ -38,5 +44,33 @@ bookApp.config(['$routeProvider',
     	return !$scope.nameFilter || keyword.test(book.name) || keyword.test(book.author);
 	};
 }]);
+
+bookApp.controller('DeatilController',function($scope,$http,$log,$routeParams){
+	//Store detail id in Controller
+	$scope.bookid = $routeParams.bookid;
+	//Initialist the book Data
+	$scope.book={};
+	//Initialise Error Handler
+	$scope.notFound = false;
+	//Do an API Call to findEmployeebyEmpnum with $routeParams.profileid
+	$http.get("http://localhost:1337/book/findBookbyNum/"+$scope.bookid)
+		 .success(function(data){
+		 	//On successful API CALL check whether empty data is returned or not
+		 	if(data.notFound === true)
+		 	{
+		 		//If book not Found set error flag -- ng-show manages the rest 
+		 		$scope.notFound = true;
+		 		return;
+		 	}
+		 	//if book found copy book Data
+		 	$scope.book=data.userData;
+		 	//Log the data
+		 	$log.info(data);
+		 })
+		 .error(function(data){
+		 	//Log error Data
+		 	$log.info(data);
+		 });
+});
 
 
