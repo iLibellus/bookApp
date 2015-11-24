a5040e51e5f60577c062be25fa067801851b7e05
Elisabet Svärd <elisabet.svard@transmode.com>
2015-11-24 17:10:31 +0100

    Further adds.

--- api/models/Book.js
+++ api/models/Book.js
@@ -14,7 +14,6 @@ module.exports = {
     author: {
       type: 'string'
     }
-
   }
 }
 
--- assets/js/app.js
+++ assets/js/app.js
@@ -7,10 +7,10 @@ bookApp.config(['$routeProvider',
       templateUrl: '/templates/book.html',
       controller: 'BookCtrl'
     }) //.when adds new route definition to $route service
-	   .when('/profile/:profileid',{
+	   .when('/bookinfo/:bookid',{
 	   			//Profile View Routing to profile.html and controller - ProfileController
-				templateUrl:'/templates/profile.html',
-				controller:'ProfileController'
+				templateUrl:'/templates/detail.html',
+				controller:'BookiInfoCtrl'
 		})
 		.otherwise({
 		  redirectTo: '/',
@@ -21,7 +21,7 @@ bookApp.config(['$routeProvider',
   bookApp.controller('BookCtrl', ['$scope', '$rootScope', 'BookService', function($scope, $rootScope, BookService) {
       $scope.formData = {};
       $scope.books = [];
-	$scope.nameFilter = null;
+	  $scope.nameFilter = null;
     
       BookService.getBooks().then(function(response) {
         $scope.books = response;
@@ -45,32 +45,19 @@ bookApp.config(['$routeProvider',
 	};
 }]);
 
-bookApp.controller('DeatilController',function($scope,$http,$log,$routeParams){
+bookApp.controller('BookiInfoCtrl',['$scope', 'BookInfoService', function($scope,$http,$log,$routeParams){
 	//Store detail id in Controller
 	$scope.bookid = $routeParams.bookid;
 	//Initialist the book Data
 	$scope.book={};
 	//Initialise Error Handler
 	$scope.notFound = false;
-	//Do an API Call to findEmployeebyEmpnum with $routeParams.profileid
-	$http.get("http://localhost:1337/book/findBookbyNum/"+$scope.bookid)
-		 .success(function(data){
-		 	//On successful API CALL check whether empty data is returned or not
-		 	if(data.notFound === true)
-		 	{
-		 		//If book not Found set error flag -- ng-show manages the rest 
-		 		$scope.notFound = true;
-		 		return;
-		 	}
-		 	//if book found copy book Data
-		 	$scope.book=data.userData;
-		 	//Log the data
-		 	$log.info(data);
-		 })
-		 .error(function(data){
-		 	//Log error Data
-		 	$log.info(data);
-		 });
-});
+
+	$scope.findBookById = function(book) {
+		BookService.findBookById(book).then(function(response) {
+			$scope.book = response;
+		})
+	}	
+}]);
 
 
